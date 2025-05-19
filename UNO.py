d5dc41d983cd509ab182c7328313fb861a67a980
arnepie <152080744+arnepie@users.noreply.github.com>
2025-05-19 21:15:45 +0200

    Update UNO.py

--- UNO.py
+++ UNO.py
@@ -17,8 +17,8 @@ def load_images(cards):
     images = {}
 
     for card in cards:
-        images[card] = pygame.image.load("images\\" + card + ".png")
-    images['back'] = pygame.image.load("images\\back.png")
+        images[card] = pygame.image.load("images/" + card + ".png")
+    images['back'] = pygame.image.load("images/back.png")
 
     return images
 
@@ -27,8 +27,17 @@ def draw_cards(images):
     screen.blit(images.get('back'), (500, 400))
 
     if GameState.pile:
-        screen.blit(images.get(GameState.pile[-1]), (600, 400))
-    
+        top_card = GameState.pile[-1]
+
+        if top_card.endswith('+4'):
+            image_key = 'black_+4'
+        elif top_card.endswith('wildcard'):
+            image_key = 'black_wildcard'
+        else:
+            image_key = top_card
+
+        screen.blit(images.get(image_key), (600, 400))
+
     for i, card in enumerate(GameState.player_hand):
         if images.get(card):
             x_pos = 100 + (100 * i)
@@ -44,15 +53,16 @@ def draw_cards(images):
 
 
 def draw_color_choosing_screen():
-    my_rectangle = pygame.Rect(400, 400, 200, 200)
-    pygame.draw.rect(screen, (255, 255, 255 ), my_rectangle)
-    blue_rectangle = pygame.Rect(100,100,50,50)
+    Background = pygame.Rect(550, 425, 450, 150)
+    blue_rectangle = pygame.Rect(575, 450, 100, 100)
+    green_rectangle = pygame.Rect(675, 450, 100, 100)
+    red_rectangle = pygame.Rect(775, 450, 100, 100)
+    yellow_rectangle = pygame.Rect(875, 450, 100, 100)
+    
+    pygame.draw.rect(screen, (255, 255, 255 ), Background)
     pygame.draw.rect(screen,(0,0,255), blue_rectangle )
-    green_rectangle = pygame.Rect(100,100,50,50)
     pygame.draw.rect(screen,(0,128,0), green_rectangle)
-    red_rectangle = pygame.Rect(100,100,50,50)
     pygame.draw.rect(screen,(255,0,0), red_rectangle)
-    yellow_rectangle = pygame.Rect(100,100,50,50)
     pygame.draw.rect(screen,(255,255,0), yellow_rectangle)
 
     pygame.display.flip()
@@ -73,11 +83,10 @@ def main():
     draw_cards(images)
 
     while running:
-        print(GameState.choose_card)
         if GameState.choose_card == True:
             draw_color_choosing_screen()
 
-        if GameState.turn == 'comp':
+        elif GameState.turn == 'comp':
             pygame.time.delay(1000)
             GameState.play_comp_move()
             draw_screen()
@@ -88,15 +97,32 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-            if event.type == pygame.MOUSEBUTTONDOWN and GameState.turn == 'player':
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = pygame.mouse.get_pos()
 
-                index = (x - 100) // 100
-                if 0 <= index < len(GameState.player_hand):
-                    selected_card = GameState.player_hand[index]
-                    GameState.play_card(selected_card)
+                if GameState.choose_card:
+                    if 575 <= x <= 675 and 450 <= y <= 550:
+                        GameState.pile[-1] = GameState.pile[-1].replace('black', 'blue')
+                    elif 675 <= x <= 775 and 450 <= y <= 550:
+                        GameState.pile[-1] = GameState.pile[-1].replace('black', 'green')
+                    elif 775 <= x <= 875 and 450 <= y <= 550:
+                        GameState.pile[-1] = GameState.pile[-1].replace('black', 'red')
+                    elif 875 <= x <= 975 and 450 <= y <= 550:
+                        GameState.pile[-1] = GameState.pile[-1].replace('black', 'yellow')
+
+                    print(GameState.pile[-1])
+
                     draw_screen()
                     draw_cards(images)
+                    GameState.choose_card = False
+
+                elif GameState.turn == 'player':
+                    index = (x - 100) // 100
+                    if 0 <= index < len(GameState.player_hand):
+                        selected_card = GameState.player_hand[index]
+                        GameState.play_card(selected_card)
+                        draw_screen()
+                        draw_cards(images)
                     
 
 
